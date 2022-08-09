5e18ae28309e521ffc7595ef5378623b378dc088
Pérez Rosado <dperez@novasolutionsystems.com>
2022-08-09 17:00:24 -0500

    Agregando metodos para eliminar usuarios

--- src/main/java/com/dennis/service/UserService.java
+++ src/main/java/com/dennis/service/UserService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.dennis.model.User;
 import com.dennis.repository.UserRepository;
@@ -46,6 +48,40 @@ public class UserService {
 		return users;
 	}
 	
+	public User deleteUserById(String id) {
+		User ActualUser = userRepository.findById(id).orElse(null);
+		userRepository.delete(ActualUser);
+		return ActualUser;
+	}
 
+	public List<User> deleteUserByAge(int age) {
+		List<User> Users = getUsersByAge(age);
+		userRepository.deleteAll(Users);
+		return Users;
+	}
+
+	public List<User> deleteUserByName(String name) {
+		List<User> Users = getUsersByName(name);
+		userRepository.deleteAll(Users);
+		return Users;
+	}
+	
+	public List<User> deleteUserByParam(String param, String value) {
+		List<User> users = Collections.emptyList();;
+		switch(param) {
+		case "name":
+			users = deleteUserByName(value);
+			break;
+		case "age":
+			int age = 0;
+			if(StringUtils.isNumeric(value) == true) {
+				age = Integer.parseInt(value);
+			}
+			users = deleteUserByAge(age);
+			break;
+		}
+		
+		return users;
+	}
 
 }
