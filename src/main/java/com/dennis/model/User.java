fc81e017ad2109b7c0d0bd427d4b75cd4e9cf1af
Pérez Rosado <dperez@novasolutionsystems.com>
2022-08-11 00:12:10 -0500

    Agregando javadoc a las clases y metodos

--- src/main/java/com/dennis/model/User.java
+++ src/main/java/com/dennis/model/User.java
@@ -2,36 +2,104 @@ package com.dennis.model;
 
 import org.springframework.data.annotation.Id;
 
+/**
+ * Clase User
+ *
+ * Contiene información de cada usuario, como lo son *el id, elnombre y su edad.
+ *
+ * @author Dennis Pérez
+ * @version 1.0
+ */
 public class User {
+	// Atributos
+
+	/**
+	 * Id del usuario
+	 */
 	@Id
 	private String id;
+	/**
+	 * Nombre del usuario
+	 */
 	private String name;
+	/**
+	 * Edad del usuario
+	 */
 	private int age;
 
-	public void setName(String n) {
-		this.name = n;
+	// Metodos publicos
+
+	/**
+	 * Asigna el nombre a un usuario
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public void setId(String n) {
-		this.id = n;
+	/**
+	 * Asigna el id a un usuario
+	 * 
+	 * @param id
+	 */
+	public void setId(String id) {
+		this.id = id;
 	}
 
-	public void setAge(int n) {
-		this.age = n;
+	/**
+	 * Asigna la edad a un usuario
+	 * 
+	 * @param age
+	 */
+	public void setAge(int age) {
+		this.age = age;
 	}
 
+	/**
+	 * Devuelve el nombre de un usuario
+	 * 
+	 * @return
+	 *         <ul>
+	 *         <li>el nombre del usuario</li>
+	 *         </ul>
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * Devuelve la edad de un usuario
+	 * 
+	 * @return
+	 *         <ul>
+	 *         <li>el edad del usuario</li>
+	 *         </ul>
+	 */
 	public int getage() {
 		return this.age;
 	}
 
+	/**
+	 * Devuelve el id de un usuario
+	 * 
+	 * @return
+	 *         <ul>
+	 *         <li>el id del usuario</li>
+	 *         </ul>
+	 */
 	public String getId() {
 		return this.id;
 	}
 
+	/**
+	 * Devuelve el objeto User en una cadena de texto
+	 * 
+	 * @return
+	 *         <ul>
+	 *         <li>User{id=id, name='name', age='age'}</li>
+	 *         </ul>
+	 */
 	public String toString() {
 		return "User{" + "id=" + this.id + ", name='" + this.name + '\'' + ", age='" + this.age + '\'' + '}';
 	}
--- src/main/java/com/dennis/service/UserService.java
+++ src/main/java/com/dennis/service/UserService.java
@@ -11,12 +11,37 @@ import org.springframework.util.ObjectUtils;
 import com.dennis.model.User;
 import com.dennis.repository.UserRepository;
 
+/**
+ * User Service
+ *
+ * Esta clase contiene todos los métodos que necesitará el UserController para
+ * recibir y ejecutar todas las peticiones.
+ *
+ * @author Dennis Pérez
+ * @version 1.0
+ */
 @Service
 public class UserService {
 
+	/**
+	 * Repositorio que utilizará el servicio para hacer la lectura y escritura en la
+	 * base de datos
+	 */
 	@Autowired
 	private UserRepository userRepository;
 
+	/**
+	 * Agrega una lista de usuarios a la base de datos
+	 * 
+	 * @param users
+	 * @return
+	 *         <ul>
+	 *         <li>null: La lista es nula, está vacía o contiene un usuario
+	 *         nulo</li>
+	 *         <li>List<User>: La lista de usuarios que fue vaidada y agregada a la
+	 *         base de datos</li>
+	 *         </ul>
+	 */
 	public List<User> createUsers(List<User> users) {
 		if (users == null || users.isEmpty() || users.contains(null) == true) {
 			return null;
@@ -24,18 +49,68 @@ public class UserService {
 		return userRepository.saveAll(users);
 	}
 
+	/**
+	 * Devuelve una lista de todos los usuarios que hay en la base de datos
+	 * 
+	 * @return
+	 *         <ul>
+	 *         <li>empty: Si no existe ningún usuario entonces devuelve una lista
+	 *         vacía</li>
+	 *         <li>List<User>: Devuelva una lista de los usuarios encontrados</li>
+	 *         </ul>
+	 */
 	public List<User> getUsers() {
 		return userRepository.findAll();
 	}
 
+	/**
+	 * Devuelve una lista de los usuarios que tengan el mismo nombre de acuerdo al
+	 * parámetro name
+	 * 
+	 * @param name
+	 * @return
+	 *         <ul>
+	 *         <li>empty: Si no existe ningún usuario entonces devuelve una lista
+	 *         vacía</li>
+	 *         <li>List<User>: Devuelva una lista de los usuarios encontrados</li>
+	 *         </ul>
+	 */
 	public List<User> getUsersByName(String name) {
 		return userRepository.findByName(name);
 	}
 
+	/**
+	 * Devuelve una lista de los usuarios que tengan la misma edad de acuerdo al
+	 * parámetro age
+	 * 
+	 * @param age
+	 * @return
+	 *         <ul>
+	 *         <li>empty: Si no existe ningún usuario entonces devuelve una lista
+	 *         vacía</li>
+	 *         <li>List<User>: Devuelva una lista de los usuarios encontrados</li>
+	 *         </ul>
+	 */
 	public List<User> getUsersByAge(int age) {
 		return userRepository.findByAge(age);
 	}
 
+	/**
+	 * Adquiere una lista de los usuarios que tengan el mismo valor de acuerdo al
+	 * parámetro value en dos diferentes casos, para nombre y edad. Recibe ambos
+	 * parámetros de tipo String y los trata diferente de acuerdo a cada caso: -Para
+	 * name, utiliza su valor en el mismo formato de entrada -Para age, verifica si
+	 * se puede convertir en un valor numérico y de ser posible lo transforma Dado
+	 * caso que no, establece la edad en 0
+	 * 
+	 * @param param, value
+	 * @return
+	 *         <ul>
+	 *         <li>empty: Si no existe ningún usuario entonces devuelve una lista
+	 *         vacía</li>
+	 *         <li>List<User>: Devuelva una lista de los usuarios encontrados</li>
+	 *         </ul>
+	 */
 	public List<User> getUsersByParam(String param, String value) {
 		List<User> users = Collections.emptyList();
 
@@ -55,6 +130,17 @@ public class UserService {
 		return users;
 	}
 
+	/**
+	 * Actualiza a un usuario de acuerdo a su id
+	 * 
+	 * @param newUser, id
+	 * @return
+	 *         <ul>
+	 *         <li>null: Si el usuario proporcionado en los parámetros es nulo o
+	 *         vacio</li>
+	 *         <li>User: Devuelve el usuario que fue actualizado</li>
+	 *         </ul>
+	 */
 	public User updateUserById(User newUser, String id) {
 		if (ObjectUtils.isEmpty(newUser) == true) {
 			return null;
@@ -63,6 +149,16 @@ public class UserService {
 		return userRepository.save(newUser);
 	}
 
+	/**
+	 * Eliminar un usuario de acuerdo a su id
+	 * 
+	 * @param id
+	 * @return
+	 *         <ul>
+	 *         <li>null: Si no se encuentra un usuario con ese id</li>
+	 *         <li>User: Devuelve el usuario que fue eliminado</li>
+	 *         </ul>
+	 */
 	public User deleteUserById(String id) {
 		User ActualUser = userRepository.findById(id).orElse(null);
 		if (ObjectUtils.isEmpty(ActualUser) == true) {
@@ -72,18 +168,54 @@ public class UserService {
 		return ActualUser;
 	}
 
+	/**
+	 * Elimina los usuarios que tengan el mismo nombre que el parámetro name
+	 * 
+	 * @param name
+	 * @return
+	 *         <ul>
+	 *         <li>empty: Si no se encuentra usuarios con ese nombre</li>
+	 *         <li>List<User>: Devuelve los usuarios que fueron eliminados</li>
+	 *         </ul>
+	 */
 	public List<User> deleteUserByName(String name) {
 		List<User> Users = getUsersByName(name);
 		userRepository.deleteAll(Users);
 		return Users;
 	}
 
+	/**
+	 * Elimina los usuarios que tengan la misma edad que el parámetro age
+	 * 
+	 * @param age
+	 * @return
+	 *         <ul>
+	 *         <li>empty: Si no se encuentra usuarios con ese nombre</li>
+	 *         <li>List<User>: Devuelve los usuarios que fueron eliminados</li>
+	 *         </ul>
+	 */
 	public List<User> deleteUserByAge(int age) {
 		List<User> Users = getUsersByAge(age);
 		userRepository.deleteAll(Users);
 		return Users;
 	}
 
+	/**
+	 * Adquiere una lista de los usuarios eliminados que tenían el mismo valor de
+	 * acuerdo al parámetro value en dos diferentes casos, para nombre y edad.
+	 * Recibe ambos parámetros de tipo String y los trata diferente de acuerdo a
+	 * cada caso: -Para name, utiliza su valor en el mismo formato de entrada -Para
+	 * age, verifica si se puede convertir en un valor numérico y de ser posible lo
+	 * transforma Dado caso que no, establece la edad en 0
+	 * 
+	 * @param param, value
+	 * @return
+	 *         <ul>
+	 *         <li>empty: Si no se elimino ningún usuario entonces devuelve una
+	 *         lista vacía</li>
+	 *         <li>List<User>: Devuelva una lista de los usuarios eliminados</li>
+	 *         </ul>
+	 */
 	public List<User> deleteUserByParam(String param, String value) {
 		List<User> users = Collections.emptyList();
 		;
